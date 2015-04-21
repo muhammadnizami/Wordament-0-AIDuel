e475e6f692fc90785e415dae7940957f9d7333a7
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-21 17:54:42 +0700

    PerintahMovePointer

--- src/PerintahMovePointer.java
+++ src/PerintahMovePointer.java
@@ -11,15 +11,41 @@ import java.awt.Point;
  */
 public class PerintahMovePointer {
     private Point dir;
-    public void PerintahMovePointer(Point _dir){
-        
+    public static final Point DIRUPLEFT = Board.DIRUPLEFT;
+    public static final Point DIRUP = Board.DIRUP;
+    public static final Point DIRUPRIGHT = Board.DIRUPRIGHT;
+    public static final Point DIRLEFT = Board.DIRLEFT;
+    public static final Point DIRRIGHT = Board.DIRRIGHT;
+    public static final Point DIRDOWNLEFT = Board.DIRDOWNLEFT;
+    public static final Point DIRDOWN = Board.DIRDOWN;
+    public static final Point DIRDOWNRIGHT = Board.DIRDOWNRIGHT;
+    
+    /**
+     * 
+     * @param _dir arah, di antara konstanta-konstanta DIRUPLEFT,DIRUP,DIRUPRIGHT,DIRLEFT,DIRRIGHT,DIRDOWNLEFT,DIRDOWN,DIRDOWNRIGHT. Selain itu, undefined behavior.
+     */
+    public PerintahMovePointer(Point _dir){
+        dir=new Point(_dir);
     }
+    
+    /**
+     * eksekusi
+     * @param GS 
+     */
     public void execute(GameState GS){
         GS.MovePointer(dir);
     }
+    /**
+     * 
+     * @return arah gerak
+     */
     public Point getDir(){
         return dir;
     }
+    /**
+     * 
+     * @param _dir arah, di antara konstanta-konstanta DIRUPLEFT,DIRUP,DIRUPRIGHT,DIRLEFT,DIRRIGHT,DIRDOWNLEFT,DIRDOWN,DIRDOWNRIGHT. Selain itu, undefined behavior.
+     */
     public void setDir(Point _dir){
         dir = _dir;
     }
