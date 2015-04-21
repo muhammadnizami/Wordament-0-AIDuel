52b930ecf640645946bdf13caaab5842c3e9ab9e
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-21 09:27:33 +0700

    GameState

--- src/GameState.java
+++ src/GameState.java
@@ -1,5 +1,6 @@
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.TreeSet;
 import java.awt.Point;
 
 /*
@@ -19,7 +20,10 @@ public class GameState {
     
     /**ctor*/
     public GameState(){
-        /* belum diimplementasi*/
+        CB = new Board();
+        kamus = new TreeSet<String>();
+        kataTerbentuk = new TreeSet<Kata>();
+        CB.StartBoard();
     }
     /**ctor
      * membuat sebuah gamestate dengan matriks kata Matkar dan kamus kata _kamus
@@ -27,14 +31,26 @@ public class GameState {
      * @param _kamus kamus kata
      */
     public GameState(char[][] MatKar, Set<String> _kamus){
-        /* belum diimplementasi*/
+        assert(MatKar.length==4);
+        for (char[] baris : MatKar) assert (baris.length==4);
+        
+        CB = new Board(MatKar);
+        kamus = _kamus;
+        CB.StartBoard();
+        
     }
     
     /**memulai/mengakhiri pembuatan kata
      * 
      */
     public void StartStopBuatKata(){
-        /* belum diimplementasi*/
+        if (CB.getSedangBuatString()){
+            String s = CB.StopDanKirimString();
+            if (kamus.contains(s))
+                kataTerbentuk.add(new Kata(s));
+        }else{
+            CB.StartBuatString();
+        }
         
     }
     
@@ -43,18 +59,25 @@ public class GameState {
      * @param _dir arah
      */
     public void MovePointer(Point _dir){
-        /* belum diimplementasi*/
+        CB.MovePointer(_dir);
         
     }
     /**memutar board*/
     public void PutarBoard(){
-        /* belum diimplementasi*/
+        CB.PutarBoard();
         
     }
-    /**mengembalikan board*/
+    /**
+     * @return Board permainan*/
     public Board GetBoard(){
-        return null;/* belum diimplementasi*/
-        
+        return CB;/* belum diimplementasi*/
+    }
+    
+    public int totalScore(){
+        int sum = 0;
+        for (Kata k : kataTerbentuk)
+            sum+= k.getScore();
+        return sum;
     }
     
 }
