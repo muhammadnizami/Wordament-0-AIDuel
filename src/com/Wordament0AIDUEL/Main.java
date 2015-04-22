cd2720491fc231ebd17b5cb4b390287ab22f0dba
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-23 00:23:39 +0700

    BERES OYE.... tapi belum ada jUnit

---
+++ src/com/Wordament0AIDUEL/Main.java
@@ -0,0 +1,35 @@
+package com.Wordament0AIDUEL;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import static java.lang.Thread.sleep;
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nim_13512501
+ */
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException{
+        JFrame mainWindow = new JFrame("Wordament-0-AIDuel");
+        
+        
+        mainWindow.add(new JPanelKonfigurasi());
+        mainWindow.setSize(500, 400);
+
+        mainWindow.setLocationRelativeTo(null);
+
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainWindow.setVisible(true);
+        
+
+    }
+}
